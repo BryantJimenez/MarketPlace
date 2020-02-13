65051050691a1c25e3d50bd5ab6ac5fcdd676e66
carlos paiva <carlospaiva1374@gmail.com>
2020-02-13 19:43:25 -0400

    Solucionando 1 de 2 errores del envio (Glovo)

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -221,6 +221,7 @@ class PaymentController extends Controller
 
     public function calculator(Request $request)
     {   
+
         try {
             $product=Product::where('slug', request('slug'))->firstOrFail();
 
@@ -234,7 +235,7 @@ class PaymentController extends Controller
                 $data = $product->stores;
                 $data = $data->first();
 
-                $sourceDir=new Address(Address::TYPE_PICKUP, $data->lat, $data->lng, $data->address);
+                $sourceDir=new Address(Address::TYPE_PICKUP, $data->lat, $data->lng, $data->address,'1st');
 
                 $destDir=new Address(Address::TYPE_DELIVERY, $lat, $lng, "Diag. 73 75", "3A");
 
@@ -264,12 +265,15 @@ class PaymentController extends Controller
             $total=number_format(request('qty')*$price+$delivery, 2, ".", "");
             $price=number_format(request('qty')*$price, 2, ".", "");
             $delivery=number_format($delivery, 2, ".", "");
-
-
-
             return response()->json(['total' => $total, 'price' => $price, 'ofert' => $ofert, 'delivery' => $delivery, 'error' => '']);
+
         } catch (Exception $e) {
             $error = $e->getMessage();
+
+            if ($error=='') {
+                $error = 'No message';
+            }
+
             return response()->json(['error' => $error]);
         }
     }
